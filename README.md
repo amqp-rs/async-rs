3248ab0aab1d4c6250ede1a97d824f6547cc35a3
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2025-08-16 19:34:28 +0200

    v0.1.0
    Initial experimental release to start toying with it and validate the
    design before 1.0

--- README.md
+++ README.md
@@ -19,6 +19,40 @@
 - tokio: enable the tokio implementation *(default)*
 - smol: enable the smol implementation
 - async-global-executor: enable the async-global-executor implementation
+- async-io: enable the async-io reactor implementation
 
 ## Example
 
+```rust
+use async_rs::{Executor, Reactor, Runtime, TokioRuntime};
+use std::{io, sync::Arc, time::Duration};
+
+async fn get_a(rt: Arc<TokioRuntime>) -> io::Result<u32> {
+    rt.clone()
+        .spawn_blocking(move || rt.block_on(async { Ok(12) }))
+        .await
+}
+
+async fn get_b(rt: Arc<TokioRuntime>) -> io::Result<u32> {
+    rt.spawn(async { Ok(30) }).await
+}
+
+async fn tokio_main() -> io::Result<()> {
+    let rt = Arc::new(Runtime::tokio());
+    let a = get_a(rt.clone()).await?;
+    let b = get_b(rt.clone()).await?;
+    rt.sleep(Duration::from_millis(500)).await;
+    assert_eq!(a + b, 42);
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> io::Result<()> {
+    tokio_main().await
+}
+
+#[tokio::test]
+async fn tokio() -> io::Result<()> {
+    tokio_main().await
+}
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,47 @@
 #![deny(missing_docs, missing_debug_implementations, unsafe_code)]
 
 //! A collection of traits and implementations to define a common interface across async runtimes
+//!
+//! ## Features
+//!
+//! - tokio: enable the tokio implementation *(default)*
+//! - smol: enable the smol implementation
+//! - async-global-executor: enable the async-global-executor implementation
+//! - async-io: enable the async-io reactor implementation
+//!
+//! ## Example
+//!
+//!use async_rs::{Executor, Reactor, Runtime, TokioRuntime};
+//! use std::{io, sync::Arc, time::Duration};
+//!
+//! async fn get_a(rt: Arc<TokioRuntime>) -> io::Result<u32> {
+//!     rt.clone()
+//!         .spawn_blocking(move || rt.block_on(async { Ok(12) }))
+//!         .await
+//! }
+//!
+//! async fn get_b(rt: Arc<TokioRuntime>) -> io::Result<u32> {
+//!     rt.spawn(async { Ok(30) }).await
+//! }
+//!
+//! async fn tokio_main() -> io::Result<()> {
+//!     let rt = Arc::new(Runtime::tokio());
+//!     let a = get_a(rt.clone()).await?;
+//!     let b = get_b(rt.clone()).await?;
+//!     rt.sleep(Duration::from_millis(500)).await;
+//!     assert_eq!(a + b, 42);
+//!     Ok(())
+//! }
+//!
+//! #[tokio::main]
+//! async fn main() -> io::Result<()> {
+//!     tokio_main().await
+//! }
+//!
+//! #[tokio::test]
+//! async fn tokio() -> io::Result<()> {
+//!     tokio_main().await
+//! }
 
 mod runtime;
 pub use runtime::*;

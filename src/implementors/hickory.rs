6cdede6798792a6b64e91e67cfa0c2dbd0a92e4e
Marc-Antoine Perennou <Marc-Antoine@Perennou.com>
2025-08-21 22:45:14 +0200

    hickory: add missing constructors

--- src/implementors/hickory.rs
+++ src/implementors/hickory.rs
@@ -1,12 +1,33 @@
 use crate::{traits::AsyncToSocketAddrs, util::SocketAddrsFromIpAddrs};
 use hickory_resolver::{IntoName, Resolver};
-use std::{io, net::SocketAddr};
+use std::{io, net::SocketAddr, str::FromStr};
 
 /// Perform async DNS resolution using hickory-dns
 #[derive(Debug)]
 pub struct HickoryToSocketAddrs<T: IntoName + Send + 'static> {
     host: T,
     port: u16,
+}
+
+impl<H: IntoName + Send + 'static> HickoryToSocketAddrs<H> {
+    /// Create a `HickoryToSocketAddrs` from split host and port components.
+    pub fn new(host: H, port: u16) -> Self {
+        Self { host, port }
+    }
+}
+
+impl FromStr for HickoryToSocketAddrs<String> {
+    type Err = io::Error;
+
+    fn from_str(s: &str) -> io::Result<Self> {
+        let (host, port_str) = s
+            .rsplit_once(':')
+            .ok_or_else(|| io::Error::new(io::ErrorKind::InvalidInput, "invalid socket address"))?;
+        let port = port_str
+            .parse()
+            .map_err(|_| io::Error::new(io::ErrorKind::InvalidInput, "invalid port value"))?;
+        Ok(Self::new(host.to_owned(), port))
+    }
 }
 
 impl<T: IntoName + Send + 'static> AsyncToSocketAddrs for HickoryToSocketAddrs<T> {
